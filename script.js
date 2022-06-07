091d20e2ff984729cd11a120ba4bab7befcfad38
Matheus Fukuda <79149808+matheushideki1234@users.noreply.github.com>
2022-06-07 14:18:24 -0300

    exerciciojapascript2

---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML =`Agora são ${hora} horas. `
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'foto-manha.jpeg'
+        document.body.style.background = '#e2cd9f'
+}
+else if (hora >= 12 && hora <= 18) {
+        img.src = 'foto-tarde.jpeg'
+        document.body.style.background = '#b9846f'
+} 
+else {
+    img.src = 'foto-noite.jpeg'
+    document.body.style.background = '#515154'
+}
+
+}
